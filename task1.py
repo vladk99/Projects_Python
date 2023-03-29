babe62255a8afb0764ef044edeb7fc5e3b066334
vladk99 <113844961+vladk99@users.noreply.github.com>
2023-03-29 23:11:21 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -3,19 +3,3 @@ print('Koshma Vladyslav','IKM-221a')
 str_name = ('Vladyslav' for _ in range(45))
 print(','.join(str_name))
 print((195 + 1,3/8))/(18,2 - (3,8 * 3,8) + 19,3)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

2df63ba5428164409fa0ccfcc070a7e227c7b3f3
vlad <vladkoshma7@gmail.com>
2023-04-17 17:32:11 +0300

    task 2 complete

---
+++ koshma_task2.py
@@ -0,0 +1,18 @@
+# Multi-paradigm movie programming
+# Lab Work 2
+# Koshma Vladyslav IKM-221a
+import math as m
+
+
+def raw_input():
+    x = float(input('User enter real number:'))
+    y = float(input('User enter real number:'))
+    z = float(input('User enter real number:'))
+    r = ((x + z) / 8) / (18.2 - m.pow(3.8, y) + 19.3)
+    print(r)
+
+
+if __name__ == '__main__':
+    print('Multi-paradigm movie programming', 'Lab Work 2')
+    print('Koshma Vladyslav', 'IKM-221a')
+    raw_input()

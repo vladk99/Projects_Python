101ed245343bece59fc41b6c62009bf07763fbf6
vladk99 <vladkoshma7@gmail.com>
2023-05-31 10:02:42 +0300

    1

--- task3.py
+++ task3.py
@@ -5,10 +5,12 @@ import math as m
 
 TEMPLATE = 'Input {} = '
 
+
 def validate_inp(variable_name, incorrect_value, incorrect_value2):
     while True:
         variable_val = inp_variable(variable_name, TEMPLATE)
-        if (variable_val != incorrect_value) and (variable_val != -incorrect_value) and (variable_val != incorrect_value2):
+        if (variable_val != incorrect_value) and (variable_val != -incorrect_value) and (
+                variable_val != incorrect_value2):
             return variable_val
         print(f'Input incorrect, {variable_name} != {incorrect_value}, {-incorrect_value}')
 
@@ -21,7 +23,6 @@ def inp_variable(values, template):
             print('Enter not number')
 
 
-
 def raw_input():
     x = validate_inp('x', y_incorrect, 0)
     if (m.log(x ** 2)) <= 4:
@@ -29,9 +30,10 @@ def raw_input():
     r = ((m.log(x ** 2)) - 4) ** 1 / 2
     print(r)
 
+
 if __name__ == '__main__':
     print('Multi-paradigm movie programming', 'Lab Work 2')
     print('Koshma Vladyslav', 'IKM-221a')
 
-    y_incorrect = 5 ** (1/2)
+    y_incorrect = 5 ** (1 / 2)
     raw_input()

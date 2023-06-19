be62a2bfc051aec0b9f14a33018524b97d50c9c9
vladk99 <vladkoshma7@gmail.com>
2023-06-20 00:11:47 +0300

    complete task 5

--- koshma_task5.py
+++ koshma_task5.py
@@ -1,3 +1,4 @@
+import utils
 import math
 
 def calculate_sum(e):
@@ -15,10 +16,29 @@ def calculate_sum(e):
 
     return sum
 
+
+def digit_count(n):
+    if n == 0:
+        digit_cout = 1
+    else:
+        digit_cout = int(math.log10(abs(n))) + 1
+    return digit_cout
+def square_root(number, e):
+    x = number  # Початкове наближення
+    while True:
+        y = (x + number / x) / 2  # Ітераційна формула Герона
+        if abs(y - x) < e:  # Перевірка точності
+            break
+        x = y
+    return y
+
 e = 1e-4
 result = calculate_sum(e)
 print(f"The sum of the series with accuracy {e} is: {result}")
 
-n = int(input("Введіть ціле число: "))
-digit_count = len(str(abs(n)))
-print(f"Кількість цифр у числі {n}: {digit_count}")
+n=utils.inp_variable("n : ", int)
+print(f"Кількість цифр у числі {digit_count(n)}")
+
+number = utils.inp_variable("Введіть число: ", float)
+result = square_root(number, e)
+print(f"Квадратний корінь з числа {number}={result}")
